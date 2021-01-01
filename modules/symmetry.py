070cdf455e030d2e25b1f561c18f6b89fa8e5768
bohdanmykhayliv <bohdan.mykhailiv@ucu.edu.ua>
2021-01-01 14:31:13 +0200

    Combine reflection and symmetry funtions into modules

--- modules/symmetry.py
+++ modules/symmetry.py
@@ -1,9 +1,36 @@
-"""Module: build_symmetry_func for Computer Discrete Math project
+"""Module: symmetry for Computer Discrete Math project
 
     Functions
     ---------
+        symmetry_check_func: function checks if relation is symmetrical
         build_symmetry_func: function builds symmetrical relation
+
 """
+
+
+def symmetry_check_func(relation):
+    """
+    Function checks if relation is reflection
+    >>> symmetry_check_func([[0, 0, 0],\
+                             [0, 1, 0],\
+                             [0, 0, 1]])
+    True
+    >>> symmetry_check_func([[0, 1, 0],\
+                             [0, 1, 0],\
+                             [0, 0, 1]])
+    False
+    """
+    t_f = []
+
+    for i in range(len(relation)):
+        for j in range(len(relation)):
+            if i != j:
+                if relation[i][j] == relation[j][i]:
+                    t_f.append(True)
+                else:
+                    t_f.append(False)
+
+    return all(t_f)
 
 
 def build_symmetry_func(relation):
@@ -25,3 +52,5 @@ def build_symmetry_func(relation):
                     relation[i][j] = 1
                     relation[j][i] = 1
     return relation
+import doctest
+doctest.testmod()
